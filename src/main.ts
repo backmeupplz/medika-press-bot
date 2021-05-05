e039ee5ed3543c0e5c47d813473df87c6b17bd99
Stas N <stas-nepomn@ya.ru>
2021-05-05 21:23:00 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await NestFactory.createApplicationContext(AppModule);
+  console.log('Medika Press bot started.');
 }
 
 bootstrap();
