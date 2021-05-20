c4e1fce0cc39836b94cd429ef244ee6d7e42fa9f
Stas N <stas-nepomn@ya.ru>
2021-05-21 00:05:29 +0800

    Translation added

--- src/modules/pubmed/pubmed.service.ts
+++ src/modules/pubmed/pubmed.service.ts
@@ -1,15 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import cheerio from 'cheerio';
 import got from 'got/dist/source';
 import { sampleSize } from 'lodash';
+const { Translate } = require('@google-cloud/translate').v2;
 import { MessageService } from '../message/message.service';
 
 @Injectable()
 export class PubmedService {
   constructor(private readonly messageService: MessageService) {}
 
-  @Cron(CronExpression.EVERY_DAY_AT_8PM)
+  @Cron(CronExpression.EVERY_10_SECONDS)
   async publishArticles() {
     try {
       // Get articles
@@ -22,12 +24,12 @@ export class PubmedService {
         const title = await this.getTitle(
           `https://pubmed.ncbi.nlm.nih.gov/${id}/`,
         );
-        articlesTitles.push(title.replace(' - PubMed', ''));
+        articlesTitles.push(title);
       }
       // Publish them
-      let publishText = '👋 New PubMed articles for today (COVID-19 week):\n\n';
+      let publishText = '👋 Новые статьи на сегодня (неделя COVID-19):\n\n';
       for (let i = 0; i < 3; i++) {
-        publishText += `📖 *${articlesTitles[i]}:* https://pubmed.ncbi.nlm.nih.gov/${articlesId[i]}/\n\n`;
+        publishText += `📖 *${articlesTitles[i]}*\n🌐 https://pubmed.ncbi.nlm.nih.gov/${articlesId[i]}/\n\n`;
       }
       await this.messageService.sendMessage(publishText);
     } catch (error) {
@@ -45,7 +47,11 @@ export class PubmedService {
   private async getTitle(link: string) {
     const response = await got(link);
     const $ = cheerio.load(response.body);
-    const title = $('head > title').text();
-    return title;
+    let title = $('head > title').text();
+    title = title.replace(' - PubMed', '');
+    const translate = new Translate();
+    const [translation] = await translate.translate(title, 'ru');
+
+    return `${title}\n📚 ${translation}`;
   }
 }
